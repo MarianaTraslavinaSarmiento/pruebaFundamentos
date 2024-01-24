70805aa2bcf26da6998efec403c2ee0bf7178416
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-01-24 12:00:45 -0500

    Ejercicio1_pruebaFundamentos

--- pruebaFundamentos.py
+++ pruebaFundamentos.py
@@ -1,1 +1,14 @@
-xmcvkjxnvc
+
+voltaje = 0
+
+for x in range(5):
+    voltajeUsuario = float(input(f"Ingresar voltaje {x+1}: "))
+    voltaje += voltajeUsuario
+
+promedio = voltaje/5
+
+if promedio > 220:
+    print ("ALTO VOLTAJE")
+
+else:
+    print("VOLTAJE CORRECTO")
